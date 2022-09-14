37bbd47106551cb8f41807d28a12c41187bbb689
alekspanteli <56685380+alekspanteli@users.noreply.github.com>
2022-09-14 14:18:58 +0300

    1 file modified

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
-for (let counter = 0; counter < 10; counter++){
-  console.log(counter);
+// for (let counter = 0; counter < 10; counter++){
+//   console.log(counter);
+// }
+
+const main = document.querySelector('main');
+let html = '';
+
+for (let i = 0; i <= 10; i++){
+  html += `<div>${i}</div>`
 }
+
+main.innerHTML = html;
